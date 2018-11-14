e2dac8bbe7c5dcac36b3f30ccc0c222a0d1b9b8b
slawa <slavko.dick@mash2.com>
2018-11-14 13:36:45 +0100

    added type change for custom type

--- Observer/ImportProduct.php
+++ Observer/ImportProduct.php
@@ -13,20 +13,52 @@ use Magento\Framework\Event\ObserverInterface;
 class ImportProduct implements ObserverInterface
 {
     const SIMPLE = 'simple';
-    const CUSTOM_TYPE = 'customType';
+    const CONFIGURABLE = 'configurable';
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $data = $observer->getTransport()->getData();
+
         $result = array();
 
-        foreach ($data as $productId => $productData) {
-            if ($productData['_type'] == self::CUSTOM_TYPE) {
-                $productData['_type'] = self::SIMPLE;
+        $typeModels = $data['type_models'];
+        $usedSkus = $data['used_skus'];
+
+        $products = array();
+
+        foreach ($data as $item) {
+            if (isset($item['sku'])) {
+                $products[] = $item;
+            }
+        }
+
+        foreach ($products as $product) {
+            if ($product['product_type'] == self::SIMPLE ||
+                    $product['product_type'] == self::CONFIGURABLE) {
+
+                $result[] = $product;
+                continue;
             }
 
-            $result[$productId] = $productData;
+            $product['attributes'][0]['cobby_custom_product_type'] = $product['product_type'];
+            $product['product_type'] = self::SIMPLE;
+
+            $result[] = $product;
         }
+
+        foreach ($typeModels as $typeModel) {
+            if ($typeModel == self::SIMPLE ||
+                    $typeModel == self::CONFIGURABLE) {
+
+                $result['type_models'][] = $typeModel;
+                continue;
+            }
+            $typeModel = self::SIMPLE;
+
+            $result['type_models'][] = $typeModel;
+        }
+
+        $result['used_skus'] = $usedSkus;
 
         $observer->getTransport()->setData($result);
 
